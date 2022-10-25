a16314a1a1f59901d1050d21377d24ce9578a6e0
klosterlight <gerry.rendon@gmail.com>
2022-10-24 20:33:27 -0500

    Allow user to get a single label

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -3,4 +3,13 @@ class LabelsController < ApplicationController
         @labels = Label.all
         render json: @labels
     end
+
+    def show
+        @label = Label.find_by id: params[:id]
+        if @label.present?
+            render json: @label
+        else
+            render json: { message: "Label not found" }, status: :not_found
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :labels, only: [:index]
+  resources :labels, only: [:index, :show]
 end
