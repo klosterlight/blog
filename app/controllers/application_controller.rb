fbdcf7529a0bde271bdcad1176aa5d7921bd697a
klosterlight <gerry.rendon@gmail.com>
2022-10-24 21:11:50 -0500

    Refactor standarize 404 messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordNotFound do |error|
+        render json: { messages: [error.message] }, status: 404
+    end
 end
