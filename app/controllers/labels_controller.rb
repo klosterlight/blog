134cfbfe2f6e04bb5ce5d60a5edc47556cc7909b
klosterlight <gerry.rendon@gmail.com>
2022-10-24 21:31:32 -0500

    Changelog

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -13,7 +13,7 @@ class LabelsController < ApplicationController
     def create
         @label = Label.new label_params
         if @label.save
-            render json: @label
+            render json: @label, status: :created
         else
             render json: { messages: @label.errors.full_messages }, status: :unprocessable_entity
         end
