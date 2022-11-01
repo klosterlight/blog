443d9703d7490bf666d3d4c3e261fa712d908ec8
klosterlight <gerry.rendon@gmail.com>
2022-10-31 19:15:37 -0600

    Added editorconfig to the project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-    rescue_from ActiveRecord::RecordNotFound do |error|
-        render json: { messages: [error.message] }, status: :not_found
-    end
+  rescue_from ActiveRecord::RecordNotFound do |error|
+    render json: { messages: [error.message] }, status: :not_found
+  end
 end
--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,45 +1,45 @@
 class LabelsController < ApplicationController
-    before_action :find_label, only: [:show, :update, :destroy]
+  before_action :find_label, only: [:show, :update, :destroy]
 
-    def index
-        @labels = Label.all
-        render json: @labels
+  def index
+    @labels = Label.all
+    render json: @labels
+  end
+
+  def show
+    render json: @label
+  end
+
+  def create
+    @label = Label.new label_params
+    if @label.save
+      render json: @label, status: :created
+    else
+      render json: { messages: @label.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    def show
-        render json: @label
+  def update
+    if @label.save label_params
+      render json: @label
+    else
+      render json: { messages: @label.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    def create
-        @label = Label.new label_params
-        if @label.save
-            render json: @label, status: :created
-        else
-            render json: { messages: @label.errors.full_messages }, status: :unprocessable_entity
-        end
-    end
+  def destroy
+    @label.destroy
 
-    def update
-        if @label.save label_params
-            render json: @label
-        else
-            render json: { messages: @label.errors.full_messages }, status: :unprocessable_entity
-        end
-    end
+    head 200
+  end
 
-    def destroy
-        @label.destroy
+  private
 
-        head 200
-    end
+  def label_params
+    params.require(:label).permit(:name, :color)
+  end
 
-    private
-    
-    def label_params
-        params.require(:label).permit(:name, :color)
-    end
-
-    def find_label
-        @label = Label.find params[:id]
-    end
+  def find_label
+    @label = Label.find params[:id]
+  end
 end
